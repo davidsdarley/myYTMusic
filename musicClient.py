bbb2f17ea3c8c6aefa99f18c3b05eba8dc288776
David <davidsdarley@gmail.com>
2025-02-06 19:02:33 -0700

    skip mostly works

--- musicClient.py
+++ musicClient.py
@@ -25,10 +25,40 @@ Client Goals:
 
 import requests
 import vlc
-import keyboard
+from pynput import keyboard
+import threading
 
 SERVER_URL = "http://127.0.0.1:5000"
+BOOMBOX = None
+STATE = True
 
+
+def press(key):
+    pass
+def release(key):
+    global STATE
+    """Called when a key is released"""
+    if key == keyboard.Key.right:
+        # If the right arrow key is released, call the skip function
+        STATE = skip()
+def skip():
+    global BOOMBOX
+    if BOOMBOX != None:
+        BOOMBOX.stop()
+        BOOMBOX = None
+        print("Skipped!")
+        return False
+    return True
+
+def controls():
+    """Listen for keyboard events in a separate thread"""
+    with keyboard.Listener(on_press = press, on_release = release) as listener:
+        listener.join()
+
+# Start the listener in a separate thread
+keyboardControls = threading.Thread(target = controls)
+keyboardControls.daemon = True  # Makes the thread exit when the main program exits. VERY IMPORTANT
+keyboardControls.start()
 def getPlaylist():
     response = requests.get("http://127.0.0.1:5000/playlist")
     if response.status_code == 200:
@@ -44,19 +74,16 @@ def getNextSong():
         return song
 
 def playAudio(url):
+    global BOOMBOX, STATE
     print(url)
     #download the audio, then play it
-    boombox = vlc.MediaPlayer(url)  # make an audioplayer object
-    boombox.play()                  # start the tunes
-    state = boombox.get_state()
-    def skip():
-        boombox.stop()
-        # sends request to server for new song
-    print()
-    while state not in [vlc.State.Ended, vlc.State.Stopped]:
-        keyboard.on_release_key("right", skip)
-        state = boombox.get_state()
-
+    BOOMBOX = vlc.MediaPlayer(url)  # make an audioplayer object
+    BOOMBOX.play()                  # start the tunes
+    while True:
+        if BOOMBOX.get_state() in [vlc.State.Ended, vlc.State.Stopped]:
+            return
+        elif STATE == False:
+            return
 
 
 
@@ -66,14 +93,16 @@ def controls():
 
 
 def main():
+    global STATE
     while True:
+        STATE = True
         songinfo = getNextSong()
         if not songinfo:
             print("No more songs")
             break
         print(songinfo[0])
         playAudio(songinfo[1])
-
+        print("DEBUG")
 
 
 if __name__ == '__main__':
